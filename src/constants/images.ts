36cce238883c9ed0d9971c65ac231bfbb13149cc
Nhan Nguyen <153412098+nhannm-vn@users.noreply.github.com>
2025-12-04 14:05:19 +0700

    #421 - config deploy

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,25 +1,23 @@
 export const LOGO_URL = {
-  white: 'https://evshare.blob.core.windows.net/images/z7141589149138_de308edc1e1d5e40f1ffa86a9476c547.jpg',
-  black: 'https://evshare.blob.core.windows.net/images/d0bc1c54-6d26-403a-a365-fd7130644e2b_1761103177179.jpg'
+  white: 'src/assets/z7049220448378_8b2ec9fc4f2ed8a19a620e26db5eb64f.jpg',
+  black: 'src/assets/z7052214352472_9110bb340043f5ad4f507f5a29909fc3.png'
 }
 
-export const REGISTER_IMG_URL =
-  'https://evshare.blob.core.windows.net/images/z7146553516164_4fc5350e827f62f34881bdf774df5fb2.jpg'
+export const REGISTER_IMG_URL = 'src/assets/ev-scaled.jpg'
 
-export const LOGIN_IMG_URL =
-  'https://evshare.blob.core.windows.net/images/z7141541067364_15cf7bc1689c0ad6fc892fc8ce86b3fa.jpg'
+export const LOGIN_IMG_URL = 'src/assets/abec5c14-53b6-4b29-bf4d-04dabd4233ec.png'
 
 export const BACKGROUNDFIXED_IMG_URL = {
-  first: 'https://evshare.blob.core.windows.net/images/z7141543487285_37cff5e2299be908f664383e52538709.jpg',
-  second: 'https://evshare.blob.core.windows.net/images/z7141542226525_5d6a5bcadfe61d7303b74e569d421895.jpg'
+  first: 'src/assets/slide/precious-madubuike-N2Td7KpIvYc-unsplash.jpg',
+  second: 'src/assets/slide/230818152324-electric-vehicle-charging-061222.jpg'
 }
 
 export const SLIDES_IMG_URL = [
-  'https://evshare.blob.core.windows.net/images/z7141543520823_70bc7888e3730695d7d3be2544fafb93.jpg',
-  'https://evshare.blob.core.windows.net/images/z7141543401435_a19e9a340bf2bb260d848d92c2e8609a.jpg',
-  'https://evshare.blob.core.windows.net/images/z7141543364000_9be80a35bc6082e3519319176564f94d.jpg',
-  'https://evshare.blob.core.windows.net/images/z7141543321611_b704bc3a8544e73c4788662a07fdeb1c.jpg',
-  'https://evshare.blob.core.windows.net/images/z7141543271868_478addab02d39844a4b0bad7a9ea4b97.jpg',
-  'https://evshare.blob.core.windows.net/images/z7141543182122_a4199060ecc9a71e9700e9c5b3cb13ee.jpg',
-  'https://evshare.blob.core.windows.net/images/z7141543108797_f1e085afca7ad885d4fee935ef9c45a5.jpg'
+  'src/assets/slide/possessed-photography-PuwfRakCJvk-unsplash.jpg',
+  'src/assets/slide/john-cameron-ftIPiEdrx2s-unsplash.jpg',
+  'src/assets/slide/possessed-photography-PuwfRakCJvk-unsplash.jpg',
+  'src/assets/slide/ralph-hutter-sBtDQMmWQPI-unsplash.jpg',
+  'src/assets/slide/red-dot-iy8h-Cl8MLc-unsplash.jpg',
+  'src/assets/slide/hannah-busing-Zyx1bK9mqmA-unsplash.jpg',
+  'src/assets/slide/Giving-Away-Money-Will-Make-You-Richer.jpg'
 ]
